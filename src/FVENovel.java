124e12ace018019d7f8586b51dddf06ecf101472
saima5 <saima5@vt.edu>
2018-03-29 17:56:09 -0400

    Added option for skipping seed generation

--- src/FVENovel.java
+++ src/FVENovel.java
@@ -13,6 +13,7 @@ public class FVENovel {
 	private static String dbDir = "";
 	private static boolean reportCoverage = false;
 	private static int spadesKmerlen = 21;
+	private static boolean generateSeeds = true;
 	
 	private static void printUsage() {
 	    System.out.println("Usage:");
@@ -61,6 +62,16 @@ public class FVENovel {
 							TOP_BINS = Integer.parseInt(args[i + 1]);
 						} else if (args[i].equals("-spadeskmer")) {
                             spadesKmerlen = Integer.parseInt(args[i + 1]);
+                        } else if (args[i].equals("-generateSeeds")) {
+                            if (args[i + 1].equals("true")) {
+                                generateSeeds = true;
+                            }
+                            else if (args[i + 1].equals("false")){
+                                generateSeeds = false;
+                            }
+                            else {
+                                generateSeeds = true;
+                            }
                         } else if (args[i].equals("-reportCov")) {
 							if (args[i + 1].equals("true")) {
 								reportCoverage = true;
@@ -87,9 +98,14 @@ public class FVENovel {
 		growScaffolds.initialize(read1, read2, outputDirName, FVEResDir, dbType, dbDir, MYTHREADS, MIN_FOLD_COV,
 				MIN_SCAFFOLD_LEN, TOP_BINS, spadesKmerlen);
 
-		System.out.println("Started 1st round of assembly: " + LocalDateTime.now());
-		growScaffolds.writeFastqFilesAndAssembly();
-		System.out.println("Finished 1st round of assembly: " + LocalDateTime.now());
+		if (generateSeeds) {
+        		System.out.println("Started 1st round of assembly: " + LocalDateTime.now());
+        		growScaffolds.writeFastqFilesAndAssembly();
+        		System.out.println("Finished 1st round of assembly: " + LocalDateTime.now());
+		}
+		else {
+		    System.out.println("Skipping 1st round of assembly");
+		}
 		
 		int totalSeedScaffolds = growScaffolds.getSeedScaffolds();
 		System.out.println("Total seed scaffolds: " + totalSeedScaffolds);
